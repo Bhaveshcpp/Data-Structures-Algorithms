edeae1bc4789d30fcbdfb48fda7e9934895297d9
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-08-11 19:40:37 +0530

    Time: 0 ms (100%), Space: 40.3 MB (61.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> ans(n, 1);
+  
 
-        int left = 1;
-        for (int i = 0; i < n; i++) {
-            ans[i] = left;
-            left *= arr[i];
+      int zeros = 0, idx = -1;
+    int prod = 1;
+
+    // Count zeros and track the index of the zero
+    for (int i = 0; i < arr.size(); ++i) {
+        if (arr[i] == 0) {
+            zeros++;
+            idx = i;
+        } else {
+            prod *= arr[i];
         }
-
-        int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] *= right;
-            right *= arr[i];
-        }
-
-        return ans;
     }
+
+    vector<int> res(arr.size(), 0);
+
+    // If no zeros, calculate the product for all elements
+    if (zeros == 0) {
+        for (int i = 0; i < arr.size(); i++)
+            res[i] = prod / arr[i];
+    }
+    // If one zero, set product only at the zero's index
+    else if (zeros == 1)
+        res[idx] = prod;
+
+    return res;
+}
 };
