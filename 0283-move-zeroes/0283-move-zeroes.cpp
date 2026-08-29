55f09fa7e48e5c62051032a0a8a2713a3f6805a7
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-08-29 17:48:28 +0530

    Time: 0 ms (100%), Space: 24 MB (7.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,9 +5,13 @@ public:
       int count=0;
       for(int i=0;i<n;i++){ 
       if(nums[i]!=0){
-           swap(nums[i] , nums[count]);
-           count++; 
-      } 
+           nums[count]=nums[i];
+           count++;
         }
+   }
+   while(count<n){
+    nums[count]=0;
+    count++;
+   }
     }
 };
