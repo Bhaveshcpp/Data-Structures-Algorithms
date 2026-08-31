92d044ea417fa3a162f9fbf15f2adbfea040b3e3
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-09-01 00:05:49 +0530

    Time: 0 ms (100%), Space: 10.8 MB (11.43%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        if(n!=m) return false;
+        unordered_map<char,int>mp(n);
+        for(char ch:s){
+            mp[ch]++;
+        }
+        for(char ch : t){
+           if(mp.find(ch)!=mp.end() && mp[ch]!=0) mp[ch]--;
+           else return false;
+        }
+     return true;
+    }
+};
