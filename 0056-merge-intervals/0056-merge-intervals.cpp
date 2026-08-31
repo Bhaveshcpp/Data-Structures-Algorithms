e7370594260037e0494b719f5f2e1a889d25a2e6
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-08-31 15:27:47 +0530

    Time: 1 ms (94.83%), Space: 23.9 MB (59.72%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
-        // Step 1: Sort by starting time
-        sort(intervals.begin(), intervals.end());
-
         vector<vector<int>> ans;
-
-        // Add the first interval
+        // sort this on the basis of first element in the sub array//
+        sort(intervals.begin(), intervals.end());
         ans.push_back(intervals[0]);
 
-        // Step 2: Process remaining intervals
+        // check overlapping-> if intervals[i][0]<=intervals[i-1][1]  then
+        // ans[i-1]
         for (int i = 1; i < intervals.size(); i++) {
-
-            // Overlapping
             if (intervals[i][0] <= ans.back()[1]) {
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            }
 
-            // Non-overlapping
-            else {
-                ans.push_back(intervals[i]);
+                  ans.back()[1]=(ans.back()[1] >=intervals[i][1])? ans.back()[1]: intervals[i][1];
             }
+            else ans.push_back(intervals[i]);
         }
-
-        return ans;
-    }
-};
+            return ans;
+        }
+    };
