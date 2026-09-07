a65c8c73efdc524a1fc108706b872ca772fd3317
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-09-08 00:40:48 +0530

    Time: 12 ms (83.12%), Space: 23.6 MB (95.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
+public:// O(n*k*log(k)) Time and O(n*k) Space
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-           vector<vector<string>> res;
+     vector<vector<string>> res;
     unordered_map<string, int> mp;
     for (int i = 0; i < strs.size(); i++) {
         string s = strs[i];
@@ -14,7 +14,8 @@ public:
         // store the index of the group in hash map
         if (mp.find(s) == mp.end()) {
             mp[s] = res.size();
-            res.push_back({});
+             res.push_back({});
+            
         }
       
         // Insert the string in its correct group
