9921bb4fb44f2f0801fa2858dbff05ce3ca172ec
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-09-11 17:32:55 +0530

    Time: 14 ms (65.24%), Space: 18.7 MB (64.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,22 +3,22 @@ public:
     int lengthOfLongestSubstring(string s) {
      //given s= abcabcbb//
      int n=s.length();
-     int ans=0;
+     int hash[256]={0};
      int left=0;
      int right=0;
-     int hash[256]={0};
+     int len=0;
      while(right<n){
-        if(hash[s[right]] == 0){
+          
+        if(hash[s[right]]==0){
             hash[s[right]]=1;
-            ans=max(ans,right-left+1);
+            len=max(len,right-left+1);
             right++;
         }
         else{
-        hash[s[left]]=0;
-        left++;
-        
-       }
+            hash[s[left]]=0;
+            left++;
+        }
      }
-      return ans;
-     } 
+    return len;
+    } 
 };
