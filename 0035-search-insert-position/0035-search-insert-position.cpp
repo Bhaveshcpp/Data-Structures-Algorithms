63009b577d63dc9f583125a09e038f3583df0740
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-09-15 16:21:12 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.53%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -7,11 +7,11 @@ public:
         while(left<=right){
             int mid=left+(right-left)/2;
             
-            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) right=mid-1;
             
             else if(nums[mid]<target)  left=mid+1;
 
-            else right=mid-1;
+            else return mid;
         }
         return right+1;
     }
