e13913fd335b424bf4aa8b01800c5249bdd1f91e
Bhaveshcpp <bhaveshsh2004@gmail.com>
2026-09-18 18:20:12 +0530

    Time: 0 ms (100%), Space: 15.2 MB (69.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-     int pivotIndex(vector<int>nums){
-      int left=0;
-      int right=nums.size()-1;
-      while(left<right){
-        int mid=left+(right-left)/2;
-        if(nums[mid]>nums[right]){
-            left=mid+1;
-        }
-        else right=mid;
-      }
-      return left;
-     }
-
-
     int search(vector<int>& nums, int target) {
-       int idx=pivotIndex(nums);
-        int left=0;
-        int right=idx-1;
+      int n=nums.size();
+      int left=0;
+      int right=n-1;
       while(left<=right){
-        int mid=left+(right-left)/2;
+        int mid=left+ (right-left)/2;
         if(nums[mid]==target) return mid;
-        else if(nums[mid]>target) right=mid-1;
-        else left = mid +1;
+        if(nums[mid]>=nums[left]){
+            if(nums[left]<=target && target < nums[mid]) right=mid-1;
+            else left=mid+1;
+        }
+        else{
+           if(nums[mid]<target && target<=nums[right]) left=mid+1;
+           else right=mid-1;
+        }
       }
-         left=idx;
-         right=nums.size()-1;
-      while(left<=right){
-        int mid=left+(right-left)/2;
-        if(nums[mid]==target) return mid;
-        else if(nums[mid]>target) right=mid-1;
-        else left = mid +1;
-      }
-return -1;
-    }
+      return -1;
+     }
 };
